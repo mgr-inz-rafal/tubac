f7f52fefb2c14f786deffcf5927f28401dcc50c7
mgr-inz-rafal <rchabowski@gmail.com>
2018-06-28 18:48:21 +0200

    Parsing no longer fails when RESTORE-ing to program line

--- 020_TuBaC/include/grammar.h
+++ 020_TuBaC/include/grammar.h
@@ -596,14 +596,14 @@ struct tbxl_grammar : qi::grammar<Iterator, Skipper>
 		DATA = qi::string("DATA") >> raw_data >> qi::eol;
 
 		RESTORE = 
-			qi::string("RESTORE")
-				[
-					boost::bind(&reactor::got_restore_to_first_data, &r)
-				]
-			|
 			qi::string("RESTORE") >> qi::int_
 				[
 					boost::bind(&reactor::got_restore, &r, ::_1)
+				]
+			|
+			qi::string("RESTORE")
+				[
+					boost::bind(&reactor::got_restore_to_first_data, &r)
 				];
 		
 		command =
